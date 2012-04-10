d2ac44f218f6058b39958b6536a7253af3165f2b
sly74fr <slymacdev@free.fr>
2012-04-10 08:49:07 +0000

    Fixed minor documentation glitch.

--- src/fr/jmmc/smprun/preference/ApplicationListSelectionView.java
+++ src/fr/jmmc/smprun/preference/ApplicationListSelectionView.java
@@ -14,8 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
- * @author lafrasse
+ * @author Sylvain LAFRASSE
  */
 public class ApplicationListSelectionView extends ApplicationListSelectionPanel implements Observer {
 
