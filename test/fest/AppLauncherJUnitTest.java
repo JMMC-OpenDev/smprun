12a95489adf8f47f730a6337f6bb7fa4157f703e
sly74fr <slymacdev@free.fr>
2013-01-21 13:35:16 +0000

    First working one !

--- test/fest/AppLauncherJUnitTest.java
+++ test/fest/AppLauncherJUnitTest.java
@@ -5,10 +5,15 @@ package fest;
 
 import fest.common.JmcsApplicationSetup;
 import fest.common.JmcsFestSwingJUnitTestCase;
+import fr.jmmc.jmcs.App;
+import fr.jmmc.smprun.HubMonitor;
 import fr.jmmc.smprun.HubPopulator;
+import fr.jmmc.smprun.preference.Preferences;
 import org.fest.swing.annotation.GUITest;
+import org.fest.swing.core.matcher.JButtonMatcher;
 import org.fest.swing.edt.GuiActionRunner;
 import org.fest.swing.edt.GuiQuery;
+import org.fest.swing.fixture.DialogFixture;
 import org.fest.swing.timing.Condition;
 import static org.fest.swing.timing.Pause.*;
 import org.fest.swing.timing.Timeout;
@@ -34,6 +39,9 @@ public class AppLauncherJUnitTest extends JmcsFestSwingJUnitTestCase {
         // disable dev LAF menu :
         System.setProperty("jmcs.laf.menu", "false");
 
+        // reset Preferences:
+        Preferences.getInstance().resetToDefaultPreferences();
+
         JmcsApplicationSetup.define(fr.jmmc.smprun.AppLauncher.class);
 
         // define robot delays :
@@ -53,8 +61,61 @@ public class AppLauncherJUnitTest extends JmcsFestSwingJUnitTestCase {
     @GUITest
     public void shouldStart() {
 
-        // waits for initialization to finish :
-        pause(new Condition("TaskRunning") {
+        // Grab Welcome message
+        final DialogFixture welcome = window.dialog();//"\"Welcome to AppLauncher !!!");
+        saveScreenshot(welcome, "al_ihm_welcome.png");
+        welcome.button(JButtonMatcher.withText("OK")).click();
+
+        // Waits for apploication initialization to finish
+        pause(new Condition("AppInitializing") {
+            /**
+             * Checks if the condition has been satisfied.
+             * @return <code>true</code> if the condition has been satisfied, otherwise <code>false</code>.
+             */
+            @Override
+            public boolean test() {
+                final boolean done = HubPopulator.isInitialized();
+                _logger.info("AppInitializing : test = {}", done);
+                return done;
+            }
+        }, LONG_TIMEOUT);
+
+        // Grab Dock window
+        saveScreenshot(window, "al_ihm.png");
+
+        _logger.info("looking for JMMC AppLauncher ...");
+
+        // Grab Monitor Window
+        saveScreenshot(getFrame("JMMC AppLauncher"), "al_ihm_popup.png");
+
+        waitHubTasks();
+
+        _logger.info("Wait for checkJnlpSampAbilities done ...");
+
+        // wait until checkJnlpSampAbilities done
+        pause(30000l);
+    }
+
+    /**
+     * Test the application exit sequence : ALWAYS THE LAST TEST
+     */
+    @Test
+    @GUITest
+    public void shouldExit() {
+        logger.severe("shouldExit test");
+
+        // TODO : handle this !
+        //window.close();
+
+        // TODO : confirmSampMessage
+        //confirmDialogDontSave();
+
+        App.exit(1);
+    }
+
+    private void waitHubTasks() {
+        // Waits for hub tasks to finish
+        pause(new Condition("HubTaskRunning") {
             /**
              * Checks if the condition has been satisfied.
              * @return <code>true</code> if the condition has been satisfied, otherwise <code>false</code>.
@@ -65,33 +126,13 @@ public class AppLauncherJUnitTest extends JmcsFestSwingJUnitTestCase {
                 return GuiActionRunner.execute(new GuiQuery<Boolean>() {
                     @Override
                     protected Boolean executeInEDT() {
-                        final boolean done = HubPopulator.isInitialized();
-                        _logger.debug("checkRunningTasks : test = {}", done);
-                        return done;
+                        final boolean idle = HubMonitor.getInstance().isIdle();
+                        _logger.debug("HubTaskRunning : test = {}", !idle);
+                        return idle;
                     }
                 });
 
             }
         }, LONG_TIMEOUT);
-
-        window.button("Aspro2").click();
-
-        _logger.debug("checkRunningTasks : exit");
-    }
-
-    /**
-     * Test the application exit sequence : ALWAYS THE LAST TEST
-     */
-    @Test
-    @GUITest
-    public void shouldExit() {
-        logger.severe("shouldExit test");
-        
-        // TODO : wait for test to finish !
-
-        window.close();
-
-        // TODO : confirmSampMessage
-        //confirmDialogDontSave();
     }
 }
